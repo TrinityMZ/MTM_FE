87d7423183ac80ef8667dc4cb7d734b45b2de343
rlarlduf20 <kky8274@naver.com>
2023-03-29 00:14:54 +0900

    First commit🎉

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  compiler: {
+    removeConsole: {
+      exclude: ["log"],
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
